bafca464e66753fa511e2c69d7b9ef4686691668
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2026-05-09 01:39:33 +0000

    ✨ [New Feature]: ウィンドウサイズ復元を有効化

--- spec-reviewer/src-tauri/src/lib.rs
+++ spec-reviewer/src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .manage(CommandState::default())
         .invoke_handler(tauri::generate_handler![
             load_workspace,
